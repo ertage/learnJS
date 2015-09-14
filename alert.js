c088cd177d626be960938d7140f04d4f3b5a9027
ertage <shv.an.vol@gmail.com>
2015-09-14 17:39:35 +0300

    first lessons

--- alert.js
+++ alert.js
@@ -1,1 +1,9 @@
-alert("Я – JavaScript!");
+var admin;
+var name;
+name = "Vaseley"
+admin = name;
+
+name = prompt("What is your name", "Nastya");
+alert("My name is " + name);
+admin = confirm("are you admin?");
+alert(admin);
